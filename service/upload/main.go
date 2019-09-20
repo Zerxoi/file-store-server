1b46d6193c6ab30243213c594f1c321ba55d938f
Xin Zou <zerxoi@163.com>
2019-09-20 16:52:14 +0800

    gin改造大体框架完成

--- service/upload/main.go
+++ service/upload/main.go
@@ -1,36 +1,35 @@
 package main
 
 import (
-	"file-store-server/handler"
+	"file-store-server/route"
 	"log"
-	"net/http"
 )
 
 func main() {
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
-	http.HandleFunc("/file/upload", handler.HTTPInterceptor(handler.UploadHandler))
-	http.HandleFunc("/file/upload/suc", handler.HTTPInterceptor(handler.UploadSucHandler))
-	http.HandleFunc("/file/meta", handler.HTTPInterceptor(handler.GetFileMetaHandler))
-	http.HandleFunc("/file/query", handler.HTTPInterceptor(handler.FileQueryHandler))
-	http.HandleFunc("/file/download", handler.HTTPInterceptor(handler.DownloadHandler))
-	http.HandleFunc("/file/update", handler.HTTPInterceptor(handler.FileMetaUpdateHandler))
-	http.HandleFunc("/file/delete", handler.HTTPInterceptor(handler.FileDeleteHandler))
-	http.HandleFunc("/file/fastupload", handler.HTTPInterceptor(handler.TryFastUploadHandler))
-	http.HandleFunc("/file/downlaodurl", handler.HTTPInterceptor(handler.DownloadURLHandler))
+	// http.HandleFunc("/file/upload", handler.HTTPInterceptor(handler.UploadHandler))
+	// http.HandleFunc("/file/meta", handler.HTTPInterceptor(handler.GetFileMetaHandler))
+	// http.HandleFunc("/file/query", handler.HTTPInterceptor(handler.FileQueryHandler))
+	// http.HandleFunc("/file/download", handler.HTTPInterceptor(handler.DownloadHandler))
+	// http.HandleFunc("/file/update", handler.HTTPInterceptor(handler.FileMetaUpdateHandler))
+	// http.HandleFunc("/file/delete", handler.HTTPInterceptor(handler.FileDeleteHandler))
+	// http.HandleFunc("/file/fastupload", handler.HTTPInterceptor(handler.TryFastUploadHandler))
+	// http.HandleFunc("/file/downlaodurl", handler.HTTPInterceptor(handler.DownloadURLHandler))
 
-	// 分块上传接口
-	http.HandleFunc("/file/mpupload/init", handler.HTTPInterceptor(handler.InitialMultipartUploadHandler))
-	http.HandleFunc("/file/mpupload/uppart", handler.HTTPInterceptor(handler.UploadPartHandler))
-	http.HandleFunc("/file/mpupload/complete", handler.HTTPInterceptor(handler.CompleteUploadHandler))
+	// // 分块上传接口
+	// http.HandleFunc("/file/mpupload/init", handler.HTTPInterceptor(handler.InitialMultipartUploadHandler))
+	// http.HandleFunc("/file/mpupload/uppart", handler.HTTPInterceptor(handler.UploadPartHandler))
+	// http.HandleFunc("/file/mpupload/complete", handler.HTTPInterceptor(handler.CompleteUploadHandler))
 
-	http.HandleFunc("/user/signup", handler.SignupHandler)
-	http.HandleFunc("/user/signin", handler.SignInHandler)
-	http.HandleFunc("/user/info", handler.HTTPInterceptor(handler.UserInfoHandler))
+	// http.HandleFunc("/user/signup", handler.SignupHandler)
+	// http.HandleFunc("/user/signin", handler.SignInHandler)
+	// http.HandleFunc("/user/info", handler.HTTPInterceptor(handler.UserInfoHandler))
 
-	err := http.ListenAndServe("localhost:8000", nil)
-	if err != nil {
-		log.Printf("Failed to start server: %s\n", err)
-	}
+	// err := http.ListenAndServe("localhost:8000", nil)
+	// if err != nil {
+	// 	log.Printf("Failed to start server: %s\n", err)
+	// }
 
+	log.Println(route.Router().Run("localhost:8000"))
 }
