b527cddadb4354acfd63528bc0a5bc60653c397d
Xin Zou <zerxoi@163.com>
2019-09-30 09:32:45 +0800

    traefik 反向代理配置完成

--- service/upload/config/config.go
+++ service/upload/config/config.go
@@ -1,7 +1,7 @@
 package config
 
 // UploadEntry 配置上传入口地址
-var UploadEntry = "localhost:28000"
+var UploadEntry = "upload.localhost"
 
 // UploadServiceHost 上传服务监听地址
 var UploadServiceHost = "0.0.0.0:28000"
